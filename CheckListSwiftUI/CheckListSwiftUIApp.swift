f585c2744729e54fb4c21b2ebe52b35beb9dcbf1
Gilbert <taya87136@.gmail.com>
2021-05-13 21:33:25 +0800

    Initial Commit

---
+++ CheckListSwiftUI/CheckListSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  CheckListSwiftUIApp.swift
+//  CheckListSwiftUI
+//
+//  Created by Hui Chih Wang on 2021/5/13.
+//
+
+import SwiftUI
+
+@main
+struct CheckListSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
